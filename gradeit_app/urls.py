73a125f856d2d34846785b2180b6d7ad82f5a7db
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-25 21:22:45 +0300

    Create gradeit_app view project view function

--- gradeit_app/urls.py
+++ gradeit_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page,name='home_page'),
-    path(r'project/new',views.submit_project,name='submit_project')
+    path('project/new',views.submit_project,name='submit_project'),
+    path('view/project/<prj_id>',views.view_project,name='view_project'),
 ]	
