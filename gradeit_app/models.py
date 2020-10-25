ffb9a47d141016c20d6c851c8fc9e1e76d6472a7
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-25 13:48:40 +0300

    Create user profile model

--- gradeit_app/models.py
+++ gradeit_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = ImageField(blank=True, manual_crop="")
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.IntegerField(max_length=10, blank=True)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+  
+
