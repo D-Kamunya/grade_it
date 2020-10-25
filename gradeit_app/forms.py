1109155520d180ee0fdcdb403862f57a79295de5
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-25 17:05:52 +0300

    Create gradeit_app project form

--- gradeit_app/forms.py
+++ gradeit_app/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Project
+from pyuploadcare.dj.forms import ImageField
 
 class SignUpForm(UserCreationForm):
 
@@ -16,3 +17,13 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email', 'password1', 'password2',)
+
+
+
+
+class ProjectForm(forms.ModelForm):
+    cover_photo = ImageField(label='')
+
+    class Meta:
+        model = Project
+        fields = ('title', 'url', 'description','cover_photo', 'technologies')
