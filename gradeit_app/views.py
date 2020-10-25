f2262917235a0ac05e8eadf705df5d5a16547f8c
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-25 12:51:51 +0300

    Create  user login view function

--- gradeit_app/views.py
+++ gradeit_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
 # Create your views here.
 def home_page(request):
     return render(request,'index.html')
@@ -21,4 +21,28 @@ def register_user(request):
         return redirect('home_page')
   else:
       form = SignUpForm() 
-  return render(request, 'registration/registration_form.html', {'form': form})    
+  return render(request, 'registration/registration_form.html', {'form': form})   
+
+
+
+def user_login(request):
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+
+            if user.is_active:
+                login(request, user)
+
+                return HttpResponseRedirect(reverse("home_page"))
+            else:
+                return HttpResponseRedirect(reverse("user_login")) 
+
+        else:
+            return HttpResponseRedirect(reverse("user_login")) 
+    else:
+        return render(request, "registration/login_form.html", context={})  
