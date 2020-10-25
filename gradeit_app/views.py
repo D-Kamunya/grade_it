2aa29c3e2d017601c534db28c5f5d834dc0f1240
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-25 11:43:57 +0300

    Create register user view function

--- gradeit_app/views.py
+++ gradeit_app/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render,redirect,reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home_page(request):
     return render(request,'index.html')
+
+
+def register_user(request):
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        messages.success(request, f'Account for username {username} successfully created.')
+        return redirect('home_page')
+  else:
+      form = SignUpForm() 
+  return render(request, 'registration/registration_form.html', {'form': form})    
