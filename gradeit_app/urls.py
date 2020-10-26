5770486410fb5b062f27f10c77d089faa7d23eb6
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-26 15:40:16 +0300

    Add my profile urlconf

--- gradeit_app/urls.py
+++ gradeit_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home_page,name='home_page'),
     path('project/new',views.submit_project,name='submit_project'),
     path('view/project/<prj_id>',views.view_project,name='view_project'),
+    path('view/my-profile',views.my_profile,name='my_profile'),
 ]	
