6a5bb3595ac6f3ce89b3452548ab7ec479c97c0b
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-26 15:33:52 +0300

    Create logic to get site of the day

--- gradeit_app/views.py
+++ gradeit_app/views.py
@@ -5,11 +5,24 @@ from .forms import SignUpForm,ProjectForm,RatingsForm
 from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Project,Rating
+import datetime
+import random
 # Create your views here.
 def home_page(request):
     projects=Project.get_all_projects()
+    today = datetime.date.today()
+    ratings= Rating.objects.all()
+    prjs= [p.project for p in ratings]
+    site_of_day=random.choice(prjs)
+    site_of_day_ratings=get_project_ratings_av(site_of_day.id)
+    
+
+
     context={
-        'projects':projects
+        'projects':projects,
+        'today':today,
+        'site':site_of_day,
+        'site_ratings':site_of_day_ratings
     }
     return render(request,'index.html',context)
 
