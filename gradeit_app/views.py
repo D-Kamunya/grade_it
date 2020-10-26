505c71affdcf7d39a990ffa02bab7a318cadd725
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-26 08:21:10 +0300

    Create get_project_ratings_av function to calc project ratings average

--- gradeit_app/views.py
+++ gradeit_app/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from .forms import SignUpForm,ProjectForm,RatingsForm
 from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project
+from .models import Project,Rating
 # Create your views here.
 def home_page(request):
     projects=Project.get_all_projects()
@@ -82,10 +82,44 @@ def submit_project(request):
 
 
 
+def get_project_ratings_av(prj_id):
+    ratings=Rating.get_project_ratings(prj_id)
+
+    if ratings:
+        design_ratings = [int(d.design) for d in ratings]
+        design_avg = round(sum(design_ratings) / len(design_ratings),2)
+
+        usability_ratings = [int(us.usability) for us in ratings]
+        usability_avg = round(sum(usability_ratings) / len(usability_ratings),2)
+
+        content_ratings = [int(content.content) for content in ratings]
+        content_avg = round(sum(content_ratings) / len(content_ratings),2)
+
+        score_avg = round((design_avg+usability_avg+content_avg)/3,2)
+
+        av_ratings={
+            'design':design_avg,
+            'usability':usability_avg,
+            'content':content_avg,
+            'score':score_avg,
+        }
+        return av_ratings
+    else:
+        av_ratings={
+        'design':0.00,
+        'usability':0.00,
+        'content':0.00,
+        'score':0.00,
+        }
+        return av_ratings
+
+   
+
+
 @login_required(login_url='/accounts/login/')
 def view_project(request,prj_id):
     project = Project.get_project_by_id(prj_id)
-    
+    ratings=get_project_ratings_av(prj_id)
     if request.method == 'POST':
         form = RatingsForm(request.POST)
         if form.is_valid():
@@ -97,7 +131,6 @@ def view_project(request,prj_id):
             content=int(request.POST.get("content"))
             score=round((design+usability+content)/3,2)
             rate.score=score
-            print(score)
             rate.save()
             messages.success(request, f'You rated this project')
             return HttpResponseRedirect(request.path_info)
@@ -106,6 +139,7 @@ def view_project(request,prj_id):
     context = {
         'project': project,
         'form': form,
+        'ratings':ratings
     }
 
     return render(request,'project.html',context)    
